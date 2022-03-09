343c45a572c6bf35b65da5b24da4f356c16da630
rpopov94 <rpopov@jinr.ru>
2022-03-09 15:02:22 +0300

    main template

---
+++ bitrix/managed_cache/MYSQL/agents/76/76ee4ad3b12ba3471b5273d26f48b51b.php
@@ -0,0 +1,7 @@
+<?
+if($INCLUDE_FROM_CACHE!='Y')return false;
+$datecreate = '001646826956';
+$dateexpire = '001646830616';
+$ser_content = 'a:2:{s:7:"CONTENT";s:0:"";s:4:"VARS";i:1646827016;}';
+return true;
+?>
---
+++ bitrix/managed_cache/MYSQL/css/s1/empty/template_501f9110a23886bb3f4b2e9655793eb5/info_v1.php
@@ -0,0 +1,1 @@
+<?php $filesInfo = ["/local/templates/empty/css/common-styles.css" => "1452613922446467",]; ?>
